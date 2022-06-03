b99f7276e5fa8e73752ae4fcbc56f4fffaa3114b
juandavidamayaardila <juandavidamayaardila@gmail.com>
2022-06-03 14:06:58 -0500

    se adiciona la clase message

--- src/main/java/com/co/sofkau/utilities/message/Message.java
+++ src/main/java/com/co/sofkau/utilities/message/Message.java
@@ -1,6 +1,11 @@
 package com.co.sofkau.utilities.message;
 
+import com.co.sofkau.utilities.Player;
+import com.co.sofkau.utilities.Question;
 import org.jboss.logging.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Message {
 
@@ -11,6 +16,46 @@ public class Message {
     }
 
     public void showWelcome() {
-        logger.info("Esta es una calculadora en Java bajo el paradigma de POO");
+        logger.info("BIENVENIDO A QUIEN QUIERE GANAR " +
+                "1 - Jugar" +
+                "2 - Ranking de juego " +
+                "3 - Reiniciar juego" +
+                "4 - Salir");
+    }
+
+    public void getUser() {
+        logger.info("Por favor digite su nombre completo.");
+    }
+
+    public void showQuestion(Question question) {
+        logger.info(question.getDescription() +
+               "A." + question.getAnswerFalse1()+
+               "B." + question.getAnswerTrue() +
+               "C." + question.getAnswerFalse2() +
+               "D." + question.getAnswerFalse4());
+    }
+
+    public void showMessageWin(Player player) {
+        logger.info("**************** Felicitaciones \" + player.getName() + \" has ganado ****************");
+    }
+
+    public void showMessageGameOver(Player player) {
+        logger.info("**************** Game Over \" + player.getName() + \"  ****************");
+    }
+
+    public void showMessageQuestionCorrect(Player player) {
+        logger.info("**************** Respuesta Correcta!!!  ****************");
+    }
+
+    public void showMessageQuestionInCorrect(Player player) {
+        logger.info("**************** Respuesta InCorrecta!!!  ****************");
+    }
+
+    public void showHistory(ArrayList<Player> listJuagadores){
+        logger.info("Rancking de juego :");
+
+        listJuagadores.stream().forEach( player -> {
+            logger.info(player.toString());
+        });
     }
 }
